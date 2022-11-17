aa3f852b058b29ca424f64a93c8f0ee72e42e9b0
Nils Winnwa <nils.winnwa@uni-paderborn.de>
2022-11-17 13:14:04 +0100

    setup.sh: Check if python3 ist installed

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #/bin/bash
 # Install and activate mollyguard
 
+if ! command -v python3 &>/dev/null
+then
+	echo "ERROR: Please install python3 by any means neccessary."
+	exit 1
+fi
+
 echo "Starting installation"
 python3 setup.py install --install-scripts /usr/local/sbin
 cp ./mollyguard.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable --now mollyguard.service
-echo "All done."
+echo "All done. If you are brave you can now run the tests:"
+echo "bash test.sh"

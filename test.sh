589fc2fba75b61764558b259f9605142118c04bf
Nils Winnwa <nils.winnwa@uni-paderborn.de>
2022-11-17 11:32:15 +0100

    Improvements Test-Suite

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ fi
 
 echo "1. Check if service is active: "
 systemctl is-active mollyguard.service
-if [ "$(systemctl is-active mollyguard.service)" != "atcive" ]
+if [ "$(systemctl is-active mollyguard.service)" != "active" ]
 then
 	echo "1. Mollyguarding is not active. Exiting."
 	exit 1
@@ -22,26 +22,30 @@ echo "Starting tests. If your computer is still running after this script everyt
 
 echo "2. Testing 'shutdown'"
 shutdown
-echo "2. 'shutdown' doesn't work. Continuing."
+echo "2. 'shutdown' doesn't work. Continuing. Any output can be ignored"
 
-echo "3. Testing 'poweroff'"
+echo "3. Testing 'shutdown now'"
+shutdown now
+echo "3. 'shutdown now' doesn't work. Continuing."
+
+echo "4. Testing 'poweroff'"
 poweroff
-echo "3. 'poweroff' doesn't work. Continuing."
+echo "4. 'poweroff' doesn't work. Continuing."
 
-echo "3. Testing 'init 0'"
+echo "5. Testing 'init 0'"
 init 0
-echo "3. 'init 0' doesn't work. Continuing."
+echo "5. 'init 0' doesn't work. Continuing."
 
-echo "4. Testing 'halt'"
+echo "6. Testing 'halt'"
 halt
-echo "4. 'halt' doesn't work. Continuing."
+echo "6. 'halt' doesn't work. Continuing."
 
-echo "5. Testing 'reboot'"
+echo "7. Testing 'reboot'"
 reboot
-echo "5. 'reboot' doesn't work. Continuing."
+echo "7. 'reboot' doesn't work. Continuing."
 
-echo "6. Testing 'init 6'"
+echo "8. Testing 'init 6'"
 init 6
-echo "6. 'init 6' doesn't work. Continuing."
+echo "8. 'init 6' doesn't work. Continuing."
 
 echo "Finished tests"

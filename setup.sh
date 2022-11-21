ad6a447318e531140cb2aa04b0cacf8eb836f365
nwinnwa <50691361+nwinnwa@users.noreply.github.com>
2022-11-21 11:52:59 +0100

    cd to directory of repo for installation

--- setup.sh
+++ setup.sh
@@ -8,7 +8,8 @@ then
 fi
 
 echo "Starting installation"
-python3 setup.py install --install-scripts /usr/local/sbin
+cd $(dirname $0)
+python3 ./setup.py install --install-scripts /usr/local/sbin
 cp ./mollyguard.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable --now mollyguard.service
